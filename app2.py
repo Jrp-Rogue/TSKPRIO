3f1da168d945773cdb52022d036f802563f5ff80
Jrp-Rogue <rhogini@gmail.com>
2025-02-04 12:11:40 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -135,9 +135,9 @@ elif choix == "Matrice d'Eisenhower":
         st.pyplot(fig)
     if "taches" in st.session_state and len(st.session_state.taches) > 0:
         matrice = classifier_taches_eisenhower(st.session_state.taches)
-        else:
+    else:
         st.error("Aucune tâche disponible pour classer.")
-        elif afficher_matrice(matrice)
+    elif afficher_matrice(matrice)
 
 # 📌 Plan d'action
 elif choix == "Plan d'Action":
