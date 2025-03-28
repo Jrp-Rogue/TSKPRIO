9752b9a7a95a28b8241b796245835f06354cd403
Jrp-Rogue <rhogini@gmail.com>
2025-03-28 12:35:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="TSKPRIO",
     version="0.1",
-    packages=find_packages(),
-    install_requires=[
-        # autres dépendances nécessaires
-    ],
+    packages=find_packages(include=["TSKPRIO"]),
 )
