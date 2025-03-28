4732cbccf653599ef1dc89c5d02c0ffe72832647
Jrp-Rogue <rhogini@gmail.com>
2025-03-28 12:32:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='TSKPRIO',  # Nom de ton projet
-    version='0.1.0',  # Version du projet
-    packages=['TSKPRIO'],  # Assure-toi que ton dossier de code s'appelle bien 'TSKPRIO' ou change ce nom
+    name="TSKPRIO",
+    version="0.1",
+    packages=find_packages(),
     install_requires=[
-        'pandas>=1.3.3',
-        'numpy',
-        'matplotlib',
-        'gitpython',
-        'streamlit==1.15.0',
-        'gdown==4.4.0',
+        # autres dépendances nécessaires
     ],
 )
